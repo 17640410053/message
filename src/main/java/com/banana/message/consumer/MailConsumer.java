1c55f0e98cf22e039b3e907377ff3293381fb0da
li.yang <534889865@qq.com>
2020-04-06 23:23:43 +0800

    邮件修复

--- src/main/java/com/banana/message/consumer/MailConsumer.java
+++ src/main/java/com/banana/message/consumer/MailConsumer.java
@@ -2,14 +2,12 @@ package com.banana.message.consumer;
 
 import com.banana.message.config.RabbitMQConfig;
 import com.banana.message.vo.MailVo;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -24,11 +22,8 @@ public class MailConsumer {
 
     @Value("${spring.mail.username}")
     private String mailFrom;
-    private JavaMailSender sender;
-
-    public MailConsumer(JavaMailSender sender) {
-        this.sender = sender;
-    }
+    @Resource
+    private JavaMailSender mailSender;
 
     @RabbitHandler
     public void consumerMailSend(String mailContext, Channel channel, Message message) {
@@ -41,7 +36,7 @@ public class MailConsumer {
             mailMessage.setFrom(mailFrom);
             log.info(mailFrom);
             mailMessage.setText(mailVo.getText());
-            sender.send(mailMessage);
+            mailSender.send(mailMessage);
         } catch (Exception e) {
             log.info("", e);
             log.info("邮件解析失败。。。");
